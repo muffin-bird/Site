b0582cfd933d3163eb208c2c73bdacf4e5b5210e
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2022-06-18 18:20:03 +0900

    add php

---
+++ CS/footer.php
@@ -0,0 +1,21 @@
+</div>
+
+<footer>
+  <hr class="footer_line">
+  <div class="w_inner">
+    <nav>
+      <ul>
+        <li><a href="/">トップ</a></li>
+        <li><a href="./service">サービス</a></li>
+        <li><a href="./price">料金案内</a></li>
+        <li><a href="./news">ニュース</a></li>
+        <li><a href="./company">会社概要</a></li>
+        <li><a href="./contact">お問い合わせ</a></li>
+      </ul>
+    </nav>
+    <div id="copyright">&copy;2022 XXX.Inc All right Reserved</div>
+  </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ CS/front-page.php
@@ -0,0 +1,65 @@
+
+
+
+
+
+<?php get_header() ;?>
+<div id="content" class="page_front w_inner">
+      <section class="front_service">
+        <div class="sec_head">サービス</div>
+        <div class="lead">ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。</div>
+        <ul class="s_box">
+          <li>
+            <div class="ttl">ホームページ制作</div>
+            <div class="descri">ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。</div>
+          </li>
+          <li>
+            <div class="ttl">ランディングページ</div>
+            <div class="descri">ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。</div>
+          </li>
+          <li>
+            <div class="ttl">SEOコンサルティング</div>
+            <div class="descri">ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。</div>
+          </li>
+        </ul>
+      </section>
+      <section class="front_news">
+        <div class="sec_head">お知らせ</div>
+        <div class="news_list">
+          <ul>
+            <li>
+              <a href="">
+                <div class="date">2022.12.28</div>
+                <div class="ttl">セミナー開催のお知らせ</div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div class="date">2022.12.10</div>
+                <div class="ttl">冬季休業のお知らせ</div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div class="date">2022.11.28</div>
+                <div class="ttl">移転のお知らせ</div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div class="date">2022.11.22</div>
+                <div class="ttl">創立記念日のため臨時休業のお知らせ</div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div class="date">2022.11.15</div>
+                <div class="ttl">ホームページリニューアルのお知らせ</div>
+              </a>
+            </li>
+            <div class="btn_stn"><a href="">一覧をみる</a></div>
+          </ul>
+        </div>
+      </section>
+    </div>
+<?php get_footer() ;?>
