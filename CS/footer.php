d4fc1687bc7bf5fddad7a032f95f154c35f09d11
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2022-06-19 10:07:34 +0900

    change template_tag

--- CS/footer.php
+++ CS/footer.php
@@ -5,12 +5,12 @@
   <div class="w_inner">
     <nav>
       <ul>
-        <li><a href="/">トップ</a></li>
-        <li><a href="./service">サービス</a></li>
-        <li><a href="./price">料金案内</a></li>
-        <li><a href="./news">ニュース</a></li>
-        <li><a href="./company">会社概要</a></li>
-        <li><a href="./contact">お問い合わせ</a></li>
+        <li><a href="<?php bloginfo('url') ;?>">トップ</a></li>
+        <li><a href="<?php bloginfo('url') ;?>/service">サービス</a></li>
+        <li><a href="<?php bloginfo('url') ;?>/price">料金案内</a></li>
+        <li><a href="<?php bloginfo('url') ;?>/news">ニュース</a></li>
+        <li><a href="<?php bloginfo('url') ;?>/company">会社概要</a></li>
+        <li><a href="<?php bloginfo('url') ;?>/contact">お問い合わせ</a></li>
       </ul>
     </nav>
     <div id="copyright">&copy;2022 XXX.Inc All right Reserved</div>
--- CS/header.php
+++ CS/header.php
@@ -2,12 +2,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="sanitize.css">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url') ;?>/sanitize.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url') ;?>/style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
-  <title>企業サイト</title>
+  <title>ホームページ制作会社</title>
   <?php wp_head(); ?>
 </head>
 
@@ -19,17 +19,17 @@
       </div>
       <nav id="main_menu" class="w_inner">
         <ul>
-          <li><a href="/">トップ</a></li>
-          <li><a href="./service">サービス</a></li>
-          <li><a href="./price">料金案内</a></li>
-          <li><a href="./news">ニュース</a></li>
-          <li><a href="./company">会社概要</a></li>
-          <li><a href="./contact">お問い合わせ</a></li>
+          <li><a href="<?php bloginfo('url') ;?>">トップ</a></li>
+          <li><a href="<?php bloginfo('url') ;?>/service">サービス</a></li>
+          <li><a href="<?php bloginfo('url') ;?>/price">料金案内</a></li>
+          <li><a href="<?php bloginfo('url') ;?>/news">ニュース</a></li>
+          <li><a href="<?php bloginfo('url') ;?>/company">会社概要</a></li>
+          <li><a href="<?php bloginfo('url') ;?>/contact">お問い合わせ</a></li>
         </ul>
       </nav>
     </div>
     <div class="header_img">
-      <img src="./img/header_img.jpg" alt="">
+      <img src="<?php bloginfo('template_url') ;?>/img/header_img.jpg" alt="image">
     </div>
   </header><!-- ここまで ヘッダー -->
   
