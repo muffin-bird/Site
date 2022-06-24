f02357d2dce1392179fb459f5eb71bf98ab6006d
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2022-06-24 20:40:52 +0900

    create service

--- CS/page-service.php
+++ CS/page-service.php
@@ -1,13 +1,43 @@
 <?php get_header() ;?>
 
-<div id="content" class="page_wrap w_inner">
+<div id="content" class="page_wrap page_service w_inner">
 
   <div id="main">
-    <div class="head"><?php the_title() ;?></div>
-    <ul>
-      <li>ホームページ制作</li>
-      <li>ランディングページ制作</li>
-      <li>SEOコンサルティング</li>
+    <div class="page_head"><?php the_title() ;?></div>
+    <ul class="s_list">
+      <li>
+        <div class="txt">
+          <div class="ttl">1. ホームページ制作</div>
+          <div class="head">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</div>
+          <div class="descri">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>
+          ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</div>
+        </div>
+        <div class="img">
+          <img src="<?php bloginfo('template_url') ;?>/img/s_img_1.jpg"  width="300" height="200" alt="">
+        </div>
+      </li>
+      <li>
+      <div class="txt">
+          <div class="ttl">2. ランディングページ制作</div>
+          <div class="head">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</div>
+          <div class="descri">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>
+          ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</div>
+        </div>
+        <div class="img">
+          <img src="<?php bloginfo('template_url') ;?>/img/s_img_2.jpg"  width="300" height="200" alt="">
+        </div>
+      </li>
+      <li>
+      <div class="txt">
+          <div class="ttl">3. SEOコンサルティング</div>
+          <div class="head">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</div>
+          <div class="descri">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>
+          ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</div>
+        </div>
+        <div class="img">
+          <img src="<?php bloginfo('template_url') ;?>/img/s_img_3.jpg"  width="300" height="200" alt="">
+        </div>
+      </li>
     </ul>
   </div>
 </div>
